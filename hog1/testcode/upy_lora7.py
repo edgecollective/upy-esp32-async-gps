7ec3ed05c06265c87fab19b31f59d007a98efb3e
Don Blair <donblair@gmail.com>
2018-08-26 10:32:11 -0400

    update

--- hog1/testcode/upy_lora7.py
+++ hog1/testcode/upy_lora7.py
@@ -562,35 +562,20 @@ sck=Pin(5)
 mosi=Pin(18)
 miso=Pin(19)
 cs = Pin(12, Pin.OUT)
-reset=Pin(13)
-
-print('hello')
-
-address=const(0x02) #-- seems constant across widgets
+#reset=Pin(13)
 
 spi=SPI(2,baudrate=5000000,sck=sck,mosi=mosi,miso=miso)
 
 resetNum=13
+
 rfm9x = RFM9x(spi, cs, resetNum, 915.0)
 
-#mybuff=bytearray(10)
-
-#cs.value(0)
-#rfm9x._read_into(address,mybuff,length=1)
-#print(mybuff[0])
-print(rfm9x._read_u8(address))
-#print(bytes(result,'utf-8'))
-
-print(rfm9x.rssi)
-#cs.value(1)
-
-#rfm9x.send('hello')
 print('listening ...')
 packet=rfm9x.receive(timeout=5.0)
 if packet is not None:
     packet_text = str(packet, 'ascii')
     print('Received: {0}'.format(packet_text))
-    
+    print("RSSI:",rfm9x.rssi)
 spi.deinit()
 
 
